cade9ec50de97a81e29552fdcd8c4de642849c6c
Dylan Strickley <dylans2499@gmail.com>
2022-03-18 19:56:15 -0400

    Added a simple hashing module

---
+++ Code/image-hash.js
@@ -0,0 +1,23 @@
+"use strict";
+let sharp = require("sharp");
+
+const DIM = 16
+
+
+/**
+ input is any valid input from https://sharp.pixelplumbing.com/api-constructor
+ output is a string of ~49 alphanumeric characters
+*/
+module.exports = async function(input) { //accepts any sharp input
+  let pixels = await sharp(input).grayscale().resize(DIM, DIM).raw().toBuffer()
+
+  let lastPixel = 255;
+  let dHash = 0n;
+  for (let pixel of pixels) {
+    dHash <<= 1n;
+    dHash |= lastPixel < pixel ? 1n : 0n;
+    lastPixel = pixel
+  }
+
+  return dHash.toString(36);
+}
