404e03b3bbff93433d2e9563f0a6e98d0f50a518
Dylan Strickley <dylans2499@gmail.com>
2022-03-19 02:40:26 -0400

    Working Form Requests No Base 64 Conversion Should Occur! (larger request size)

--- Code/mathpix.js
+++ Code/mathpix.js
@@ -1,36 +1,32 @@
 "use strict";
-const axios = require('axios').default;
+const axios = require("axios").default;
+const FormData = require("form-data");
+const fs = require("fs/promises");
+const imageHash = require("./image-hash.js");
 
-exports.makeRequest = function(imageURI) {
-  let options = simpleOptions(imageURI);
-  
-  axios.post('https://api.mathpix.com/v3/text', options, { headers: {app_id: process.env.MATHPIX_APP_ID, app_key: process.env.MATHPIX_APP_KEY} })
-}
+const mathpixOptions = JSON.stringify({
+  //see more options at https://docs.mathpix.com/#request-parameters
+  formats: ["text", "data"],
+  //IDK if we want tsv but it could be nice.
+  data_options: {include_latex: true, include_tsv: true, include_asciimath: true},
+  //we want math
+  numbers_default_to_math: true,
+  include_line_data: true,
+  // simplify format
+  rm_spaces: true,
+  rm_fonts: true,
+});
 
-function simpleOptions(imageURI) {
-  let result = {
-    //see more options at https://docs.mathpix.com/#request-parameters
-    src: imageURI,
-    formats: ["text", "data"],
-    //IDK if we want tsv but it could be nice.
-    data_options: {include_latex: true, include_tsv: true},
-    //we want math
-    numbers_default_to_math: true,
-    //simplify format
-    rm_spaces: true,
-    rm_fonts: true,
-    //Disable other alphabets
-    alphabets_allowed: {
-    hi: false,
-    zh: false,
-    ja: false,
-    ko: false,
-    ru: false,
-    th: false,
-    ta: false,
-    te: false,
-    gu: false,
-    bn: false,
-    vi: false}
-  }
+
+exports.makeRequest = async function(imageBuffer, imageName) {
+  let form = new FormData()
+  form.append("options_json", mathpixOptions);
+  form.append("file", imageBuffer, imageName);
+  let hash = await imageHash(imageBuffer)
+  console.log("Image Hash: " + hash);
+
+ let response = await axios.post("https://api.mathpix.com/v3/text",
+     form, {headers: {...form.getHeaders(), app_id: process.env.MATHPIX_APP_ID, app_key: process.env.MATHPIX_APP_KEY} });
+
+  return response;
 }
