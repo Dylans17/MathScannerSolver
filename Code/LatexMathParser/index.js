7eaa0579e7074b31937ca8ff51baba1d957727e4
Dylan Strickley <Dylans17@users.noreply.github.com>
2022-03-30 01:58:48 -0400

    Finished merging backend with one breaking change

---
+++ Code/LatexMathParser/index.js
@@ -0,0 +1,18 @@
+import antlr4 from "antlr4";
+import LatexMathLexer from "./antlr/LatexMathLexer.js";
+import LatexMathParser from "./antlr/LatexMathParser.js";
+import LatexMathListener from "./antlr/LatexMathListener.js";
+import AstBuilderVisitor, {Node} from "./AstBuilderVisitor.js"
+
+
+export default function parseMath(str) {
+  const chars = new antlr4.InputStream(str);
+  const lexer = new LatexMathLexer(chars);
+  const tokens = new antlr4.CommonTokenStream(lexer);
+  const parser = new LatexMathParser(tokens);
+  parser.buildParseTrees = true;
+  const tree = parser.start();
+  const v = new AstBuilderVisitor();
+  const result = v.visit(tree);
+  return result;
+}
---
+++ Code/mathpix.js
@@ -0,0 +1,45 @@
+"use strict";
+import axiosLib from "axios";
+import FormData from "form-data";
+import fs from "fs/promises";
+import imageHash from "./image-hash.js";
+import { LocalStorage } from "node-localstorage";
+
+let axios = axiosLib.default;
+let imageStorage = new LocalStorage("./image-results");
+
+
+const mathpixOptions = JSON.stringify({
+  //see more options at https://docs.mathpix.com/#request-parameters
+  formats: ["text", "data"],
+  //IDK if we want tsv but it could be nice.
+  data_options: {include_latex: true, include_tsv: true, include_asciimath: true},
+  //we want math
+  numbers_default_to_math: true,
+  include_line_data: true,
+  // simplify format
+  rm_spaces: true,
+  rm_fonts: true,
+});
+
+
+export default async function makeRequest(imageBuffer, imageName) {
+  let form = new FormData();
+  form.append("options_json", mathpixOptions);
+  form.append("file", imageBuffer, imageName);
+
+  let hash = await imageHash(imageBuffer);
+  let storedResult = imageStorage.getItem(hash);
+  if (storedResult != null) {
+    if (1 /*put condition for verbose logging?*/)
+      console.log("Using stored result for new image: " + hash);
+    return JSON.parse(storedResult);
+  }
+
+  if (1 /*put condition for verbose logging?*/)
+    console.log("Fetching result for new image from mathpix: " + hash);
+  let response = await axios.post("https://api.mathpix.com/v3/text",
+     form, {headers: {...form.getHeaders(), app_id: process.env.MATHPIX_APP_ID, app_key: process.env.MATHPIX_APP_KEY} });
+  imageStorage.setItem(hash, JSON.stringify(response.data));
+  return response;
+}
