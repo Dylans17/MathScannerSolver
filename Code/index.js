2cc75c2924208081b586dfbd744cf3ee21df01e5
Dylan Strickley <Dylans17@users.noreply.github.com>
2022-04-03 08:37:03 -0400

    Simple Equation Evalutation with the Frontend

--- Code/index.js
+++ Code/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import express from 'express';
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
+import parseMath from "./LatexMathParser/index.js"
+import {approxEvaluate} from "./solver.js";
 
 const app = express();
 dotenv.config();
@@ -24,9 +26,29 @@ for (let token of envTokens) {
 }
 if (!envCorrect) {process.exit(1);}
 
+//static files use build directory first
 app.use(express.static(__dirname + "/build"));
 
-app.get('/*', function (req, res) {
+//the user can request us to parse and solve an equation /equation
+app.get("/equation", (req, res) => {
+  console.log(req.query);
+  let response = {};
+  try {
+    let equation = req.query.equation;
+    let node = parseMath(equation);
+    let result = approxEvaluate(node);
+    response = {result: result};
+  } catch (e) {
+    console.log("We got an error!");
+    console.log(e);
+    response = {error: e};
+  }
+  res.json(response);
+})
+
+//other generic requests like /type, /result, ect. go to index.html
+app.get("/*", (req, res) => {
+  console.log("requested" + req.url);
   res.sendFile(__dirname + "/build/index.html");
 });
 
--- Code/src/components/TypeEquation/type.component.jsx
+++ Code/src/components/TypeEquation/type.component.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./type.styles.css";
 import { useNavigate } from "react-router-dom";
 import { addStyles, EditableMathField, StaticMathField } from "react-mathquill";
+import axios from "axios";
 
 addStyles();
 
@@ -9,13 +10,12 @@ function TypeEquation(props) {
   let navigate = useNavigate();
   const [latex, setLatex] = useState("");
 
-
   useEffect(()=> {
     console.log(latex)
   }, [latex])
 
   return (
-    <div className="type-section">    
+    <div className="type-section">
       <section className="first-block-type-equation">
         <h3 className="type-title"> Type your equation below </h3>
         <EditableMathField className="editor"
@@ -25,14 +25,19 @@ function TypeEquation(props) {
         }}
         latex={latex}
         onChange={(mathField) => {
-          setLatex(mathField.latex());
+          setLatex(mathField.latex()); //what does this do? Does it set the text below? How does it work?
         }}
-      />     
-      {latex}    
+      />
+      {latex}
         <button
           className="submit-type-equation button"
-          onClick={(e) => navigate("/result")}
-        >        
+          onClick={(e) => {
+            //I have no clue how to read the values from React so I'm just prompting for now.
+            axios.get("./equation",{params: {equation: prompt("Enter an equation:\nThis is really bad UI haha")}})
+              .then(e=>{console.log(e)});
+            return navigate("/result");
+          }}
+        >
           Submit
         </button>
       </section>
