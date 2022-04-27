169726a9015eec52598f0e87bde90a477eee23eb
Pam <pamellabviana@gmail.com>
2022-04-27 11:55:46 -0400

    Now you can edit the equation coming from camera

--- Code/src/components/Camera/PictureTaken/picture_taken.component.jsx
+++ Code/src/components/Camera/PictureTaken/picture_taken.component.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./picture_taken.styles.css";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import Loading from "../../Loading/loading.component.jsx";
 
 export default function PictureTaken({ capturedImg }) {
   let navigate = useNavigate();
   const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function urltoFile(url, filename, mimeType) {
     return fetch(url)
@@ -18,15 +20,21 @@ export default function PictureTaken({ capturedImg }) {
   }  
 
   function post(image) {
+    setLoading(true);
     const formData = new FormData();
     formData.append('file', image);
+    const images = []
+    images.push(image); 
     axios
       .post("/input-picture", formData)
       .then((response) => {
+        setLoading(false);
         console.log("Response inside fileupload: ", response.data);
-        navigate("/result", { state: { data: response.data } });
+        //navigate("/result", { state: { data: response.data } }); 
+        navigate("/uploaded-pictures", { state: { data: response.data, image: images} });
       })
       .catch((e) => {
+        setLoading(false);
         setErrorMessage(e.response.data);
         console.log(e.response.data);
       });
@@ -54,6 +62,7 @@ export default function PictureTaken({ capturedImg }) {
           Next
         </button>
         <p className="error__message">{errorMessage}</p>
+        {loading ? <Loading /> : ""}
       </div>
     </>
   );
